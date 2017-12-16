85a243e43de2c4cd0eacaade493b9988a822f55c
Leonidas <le0199643@gmail.com>
2017-12-16 20:00:55 +0200

    Test

--- GymManagment/NewMember.cs
+++ GymManagment/NewMember.cs
@@ -28,18 +28,19 @@ namespace GymManagment
             SqlCommand da1;
             SqlDataReader r1;
             cn = new SqlConnection(cs);
-            da1 = new SqlCommand("insert into [dbo].[Customers] ([Id], [name], [surname]) values('1','yo','hi')", cn);           
+            da1 = new SqlCommand("insert into [dbo].[Customers] ([Id], [name], [surname]) values('10','to','ri')", cn);           
             try
             {
-                cn.Open();
+                cn.Open();           
                 r1 = da1.ExecuteReader();
-                MessageBox.Show("saved");
-                while (r1.Read()) { }
+                MessageBox.Show("saved");              
+                while (r1.Read()) { }             
             }
             catch(Exception ex)
             {
                 MessageBox.Show(ex.ToString());
             }
+           
             cn.Close();
         }
 
